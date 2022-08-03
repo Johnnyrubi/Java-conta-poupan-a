e80425e0f9a310f65c9994714fe7560c466832a4
johnny <johnnyrubischreiner@gmail.com>
2022-08-02 21:59:22 -0300

    concluido

--- src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,27 @@
 package com.trybe.contapoupanca;
 
 public class ContaPoupanca {
+  private double saldo;
+  private String titularConta;
 
+  ContaPoupanca(String nome, int dinheiro) {
+    this.saldo = dinheiro;
+    this.titularConta = nome;
+  }
+
+  public void depositar(double dinheiro) {
+    this.saldo += dinheiro;
+  }
+
+  public double mostrarSaldo() {
+    return this.saldo;
+  }
+
+  public void sacar(double dinheiro) {
+    this.saldo -= dinheiro;
+  }
+
+  public String mostrarTitularConta() {
+    return this.titularConta;
+  }
 }
